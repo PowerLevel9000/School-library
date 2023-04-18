2efef1348d42720ff2af48da88b07b08791daa6d
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-18 13:43:30 +0530

    attr_reader for id

--- person.rb
+++ person.rb
@@ -1,5 +1,6 @@
 class Person
-  attr_accessor :id, :name, :age
+  attr_accessor :name, :age
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = rand(1..100)
@@ -25,7 +26,7 @@ end
 
 adarsh = Person.new(21, 'Adarsh', parent_permission: false)
 can_use_services = adarsh.can_use_services?
-puts can_use_services, adarsh.age, adarsh.id, adarsh.parent_permission?, adarsh.name
+puts can_use_services, adarsh.age, adarsh.id, adarsh.parent_permission?, adarsh.name, adarsh.id 
 
 #  we can't find use of_age since it is private funtion so below line give you the error
 # puts of_age?
