325fe26ca9cb4f3dc8ad16109917c5664dfcc1fa
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-18 13:21:18 +0530

    Rubocop Fix

--- Student.rb
+++ Student.rb
@@ -1,17 +1,17 @@
 require './person'
 class Student < Person
-    attr_accessor :classroom
-    def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-        super(name, age, parent_permission: parent_permission)
-        @classroom = classroom
-    end
-    
-    def play_hooky
-        '¯(ツ)/¯'
-    end
+  attr_accessor :classroom
+
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end
 
-akash = Student.new(16, 12, "Akash")
-puts  "########################## Students ruby file #############################3"
+akash = Student.new(16, 12, 'Akash')
+puts '########################## Students ruby file #############################3'
 puts akash.name, akash.can_use_services?, akash.parent_permission?, akash.classroom, akash.play_hooky
-
