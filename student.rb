9930c8996493df87fe9ca4ef7064dca598d27a87
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-20 21:19:09 +0530

    Rubocop Linting

--- student.rb
+++ student.rb
@@ -1,7 +1,5 @@
 require './person'
 class Student < Person
-  attr_accessor :classroom
-
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
