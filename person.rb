c2fa99d5464fe988192dc988981bd7ed454d0f6f
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-20 09:56:31 +0530

    Cloud Rubocop linting error

--- person.rb
+++ person.rb
@@ -33,7 +33,6 @@ end
 
 puts '########################## person.rb file #############################'
 
-
 adarsh = Person.new(21, 'Adarsh', parent_permission: false)
 can_use_services = adarsh.can_use_services?
 puts can_use_services, adarsh.age, adarsh.id, adarsh.parent_permission?, adarsh.name, adarsh.id
