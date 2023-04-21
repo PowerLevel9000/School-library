80c07b7a8713b40b258a4a6b3d7567a7634ba5ad
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-21 20:20:06 +0530

    basic puts terminal ui

---
+++ main.rb
@@ -0,0 +1,17 @@
+def ui
+  input = 0
+  while input != 7
+    puts "********************* welcome to School Library ********************"
+    puts '1. List all books'
+    puts '2. List all books'
+    puts '3. List all books'
+    puts '4. List all books'
+    puts '5. List all books'
+    puts '6. List all books'
+    puts '7. List all books'
+    userInput = gets.chomp().to_i;
+    input = userInput
+  end
+end
+ui
+
