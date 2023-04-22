2c81bbfb5ac017f9897a4375c1ce625b26ce45dd
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-22 07:02:44 +0530

    improved ui of the console

--- app.rb
+++ app.rb
@@ -15,11 +15,11 @@ class App
 
   def list_all_books
     if @books.length.positive?
+			puts '----------------------------------'
       @books.each do |book|
-				puts '-----------------------------------------'
-        puts "#{book.title} by #{book.author}"
-				puts '-----------------------------------------'
+        puts "#{book.title.upcase} by #{book.author.upcase}"
       end
+			puts '----------------------------------'
     else
 			puts '-----------------------------------------'
       puts 'we do not have any book please create one'
@@ -31,17 +31,16 @@ class App
   def list_all_person
     puts '****** List of persons *******'
     if @person.length.positive?
+			puts '-------------------------------------------------------------------------------------'
       @person.each do |person|
         if person.respond_to?('specialization')
-					puts '-------------------------------------------------------------------------------------'
           puts "#{person.name}, he is #{person.age} years old, specilites in #{person.specialization} "
-					puts '-------------------------------------------------------------------------------------'
         else
-					puts '-----------------------------------------------------------------------------'
-          puts "#{person.name}, he is #{person.age} years old, study in #{person.classroom}"
-					puts '-------------------------------------------------------------------------------'
+          puts "#{person.name}, he is #{person.age} years old, study in #{person.classroom.label}"
         end
       end
+			puts '-------------------------------------------------------------------------------------'
+
     else
 			puts '----------------------------------------------------'
       puts "we got a problem we don't have anyone in our school"
@@ -51,105 +50,120 @@ class App
 
   def create_a_person
 		puts "******* Select another option ***********"
-    puts '1. To create a studen '
+    puts '1. To create a student'
 		puts '2. To create a teacher'
+		print 'Enter the option : '
     person_category = gets.chomp.to_i
-    print 'Enter name:'
+		if person_category < 1 or person_category > 2
+			puts '----------------------------------'
+      puts 'invalid option for creating person'
+			puts '----------------------------------'
+			return
+		end
+    print 'Enter name: '
     name = gets.chomp
-    print 'Enter age:'
+    print 'Enter age: '
     age = gets.chomp
     detial_person(person_category, name, age)
   end
 
   def detial_person(category, name, age)
     if category == 1
-      print 'Enter classroom:'
+      print 'Enter classroom: '
       classroom = gets.chomp
-      print 'Have parent permission? Y/N:'
+      puts 'Have parent permission? Y/N: '
       parent_permission = gets.chomp
       parent_permission = parent_permission.downcase == 'y'
       classroom = Classroom.new(classroom)
-      fresh_student = Student.new(classroom, age, name, parent_permission: parent_permission)
+      fresh_student = Student.new(age, classroom, name, parent_permission: parent_permission)
       @person.push(fresh_student)
-			puts '-----------------------------------------'
+			puts '---------------------------------'
       puts 'New student created successfully'
-			puts '-----------------------------------------'
-    elsif category == 2
-      print 'Enter specialization:'
+			puts '---------------------------------'
+    else
+      print 'Enter specialization: '
       specialization = gets.chomp
-      new_teacher = Teacher.new(age, name, specialization)
+      new_teacher = Teacher.new(age, specialization, name)
       @person.push(new_teacher)
 			puts '----------------------------------'
       puts 'New teacher created successfully'
 			puts '----------------------------------'
-    else
-			puts '----------------'
-      puts 'invalid option'
-			puts '----------------'
     end
   end
 
   def create_a_book
-    print 'Enter the title of the book:'
+    print 'Enter the title of the book: '
     title = gets.chomp
-    print 'Enter the author of the book:'
+    print 'Enter the author of the book: '
     author = gets.chomp
     new_book = Book.new(title, author)
     @books.push(new_book)
-    puts "created book successfully with title of #{new_book.title} authored by #{new_book.author.upcase}"
+		puts '--------------------------------------------------------------------------------------------------------'
+    puts "created book successfully with title of #{new_book.title.upcase} authored by #{new_book.author.upcase}"
+		puts '---------------------------------------------------------------------------------------------------------'
+
   end
 
   def create_rental
     if @books.empty? or @person.empty?
-      puts 'There are no books available or registered person at the moment'
+			puts '------------------------------------------------------------------------'
+      puts 'There are  books available or registered person at the moment for rental'
+			puts '------------------------------------------------------------------------'
       return
     end
-    puts 'Select a book from the shelf from the following:'
-
+		puts "******* Select option ***********"
+    puts 'Select a book from the shelf: '
+    puts ''
+		puts '=============== book self ==============='
     @books.each_with_index do |book, index|
       puts "#{index} Title #{book.title}, authored by #{book.author}"
     end
+		puts '=============== ************ ==============='
     choice_book = gets.chomp
     choice_book = @books[choice_book.to_i]
-    puts 'Select a person to rent the book by id'
+		puts "******* Select option ***********"
+    puts 'Select a person to rent the book by id: '
+    puts ''
+		puts '=============== persons ==============='
+
     @person.each_with_index do |person, index|
       puts "#{index} #{person.name.upcase} with id #{person.id} and #{person.age} years old"
     end
-
+		puts '=============== ******* ==============='
     choice_person = gets.chomp
     choice_person = @person[choice_person.to_i]
-
-    puts 'Enter date of rental to keep track of book'
-
+    print 'Enter date of rental to keep track of book : '
     date = gets.chomp
     rental = Rental.new(date, choice_book, choice_person)
-
-    puts "#{rental} record for rented book saved successfully"
+		puts '------------------------------------------------------------------------------'
+    puts "#{rental.person} record for #{rental.book} has been created saved successfully"
+		puts '------------------------------------------------------------------------------'
   end
 
   def list_all_rentals_by_id
+		puts "******* Select option i.e. person id ***********"
+		puts '========================== person ======================'
     @person.each do |person|
       puts "ID: #{person.id}, Name: #{person.name.upcase} and Age #{person.age}"
     end
-
+		puts '========================== ******* ======================'
     if @person.empty?
+			puts '----------------------------------------------------'
       puts 'No rental record in the library, you can create one.'
+			puts '----------------------------------------------------'
       return
-
     end
-
-    puts 'Enter person ID:'
-
+    print 'Enter person ID: '
     id = gets.chomp.to_i
     person = @person.find { |item| item.id == id }
-    puts 'Book rentals'
-
+		puts "========================== rentals by #{person.name.upcase} ==============================="
     if person and person.rentals.length.positive?
       person.rentals.each do |item|
-        puts "Name: #{person.name.upcase} Book: #{item.book.title} authored by #{item.book.author} on the date #{item.date}"
+        puts "Book: #{item.book.title.upcase} authored by #{item.book.author.upcase} on the date #{item.date}"
       end
     else
       puts 'No rentals available at the moment'
     end
+		puts '========================== ***************************************** ======================'
   end
 end
--- main.rb
+++ main.rb
@@ -9,28 +9,46 @@ def ui
   puts "===================================================================="
   while input[-1].to_i != 7
     puts "******* Select option ***********"
-    puts '1. List all books'
+    puts '1. List all book'
     puts '2. List all person'
-    puts '3. creat person'
-    puts '4. create books'
-    puts '5. create rental'
+    puts '3. Create person'
+    puts '4. Create book'
+    puts '5. Create rental'
     puts '6. List all rentals for a given person ID'
     puts '7. List all books'
-    userInput = Readline.readline("enter you option : ", true);
+    userInput = Readline.readline("Enter you option : ", true);
     input = userInput
     case userInput[-1].to_i
     when 1
       app.list_all_books
+      puts ''
+      puts '////////////////////// task end ///////////////////////'
+      puts ''
     when 2
       app.list_all_person
+      puts ''
+      puts '////////////////////// task end ///////////////////////'
+      puts ''
     when 3
       app.create_a_person
+      puts ''
+      puts '////////////////////// task end ///////////////////////'
+      puts ''
     when 4
       app.create_a_book
+      puts ''
+      puts '////////////////////// task end ///////////////////////'
+      puts ''
     when 5
       app.create_rental
+      puts ''
+      puts '////////////////////// task end ///////////////////////'
+      puts ''
     when 6
       app.list_all_rentals_by_id
+      puts ''
+      puts '////////////////////// task end ///////////////////////'
+      puts ''
     when 7
       puts "thanks for using"
     else
