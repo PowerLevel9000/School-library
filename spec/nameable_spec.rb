edd39032e4960b9ca7f6c22cbd7f961419f31d94
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-28 16:01:09 +0530

    namable spec typos fix and rubocop

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe Nameable do
-    let(:nameable) { Nameable.new() }
-    it "it should have a raise since no name there" do
-        expect(nameable.correct_name)to raise_error(NotImplementedError)
-    end
+  let(:nameable) { Nameable.new }
+  let(:person) { Person.new(33, 'Burak', parent_permission: false) }
+  it 'it should have a raise since no name there' do
+    expect(nameable.correct_name).to raise_error(NotImplementedError)
+  end
+
+  it 'it should return correct name ' do
+    expect(person.correct_name).to eq('Burak')
+  end
 end
