65e30b6b4978752e71e05314c940876ee08f4202
otmanTR <r.brk.otman@gmail.com>
2023-04-28 08:25:48 +0300

    add tests to student_spec.rb file

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,1 +1,27 @@
 require 'spec_helper'
+
+describe Student do
+let(:student) { Student.new(17, 'Web Dev', 'Berkay', parent_permission: true)}
+#let(:classroom) {Classroom.new('Web Dev')}
+
+describe "#initialize" do
+    it "sets the students age, classroom, name and parent_permission" do
+        expect(student.age).to eql(17)
+        expect(student.classroom).to eql('Web Dev')
+        expect(student.name).to eql('Berkay')
+        expect(student.parent_permission).to eql(true)
+    end
+end
+
+describe "#play_hooky" do
+    it "check student play_hooky" do
+        expect(student.play_hooky).to eql('¯(ツ)/¯') 
+    end
+end
+
+describe "#classroom" do
+    it "check students classroom" do
+        expect(student.classroom).to eql('Web Dev') 
+    end
+end
+end
