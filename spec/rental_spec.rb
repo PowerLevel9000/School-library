04d5d92443b0d59a34f7c1520ea286e05bacc317
otmanTR <r.brk.otman@gmail.com>
2023-04-28 11:58:21 +0300

    Implement DRY to teacher_spec.rb and checked linters

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe Rental do
-	it "sets the rental date,book,person" do
-		date = '2022-01-01'
-		book = { 'title' => 'Transformation', 'author' => 'Kafka', 'rentals' => [] }
-		person = { 'name' => 'Burak', 'age' => 25, 'parent_permission' => true, 'rentals' => [] }
-		rental = Rental.new(date, book, person)
-		expect(rental.date).to eq date
-		expect(rental.book).to eq book
-		expect(rental.person).to eq person
-	end
+  it 'sets the rental date,book,person' do
+    date = '2022-01-01'
+    book = { 'title' => 'Transformation', 'author' => 'Kafka', 'rentals' => [] }
+    person = { 'name' => 'Burak', 'age' => 25, 'parent_permission' => true, 'rentals' => [] }
+    rental = Rental.new(date, book, person)
+    expect(rental.date).to eq date
+    expect(rental.book).to eq book
+    expect(rental.person).to eq person
+  end
 end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,23 +1,16 @@
 require 'spec_helper'
 
 describe Teacher do
+  let(:teacher) { Teacher.new(33, 'Philosophy', 'Burak', parent_permission: true) }
 
-    let(:teacher) { Teacher.new(33, 'Philosophy', 'Burak', parent_permission: true)}
+  it 'sets the teacher age, speciliziation, name and parent_permission' do
+    expect(teacher.age).to eql(33)
+    expect(teacher.specialization).to eql('Philosophy')
+    expect(teacher.name).to eql('Burak')
+    expect(teacher.parent_permission).to eql(true)
+  end
 
-
-describe "#initialize" do
-    it "sets the teacher age, speciliziation, name and parent_permission" do
-        expect(teacher.age).to eql(33)
-        expect(teacher.specialization).to eql('Philosophy')
-        expect(teacher.name).to eql('Burak')
-        expect(teacher.parent_permission).to eql(true)
-    end
-end
-
-describe "#can_use_services?" do
-    it "check teacher can use service?" do
-        expect(teacher.can_use_services?).to eql(true)
-    end
-end
-
+  it 'check teacher can use service?' do
+    expect(teacher.can_use_services?).to eql(true)
+  end
 end
