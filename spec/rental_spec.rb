9861f197f909b218dd748ae866bdadf19ab408d8
otmanTR <r.brk.otman@gmail.com>
2023-04-28 09:03:59 +0300

    update reantal_spec.rb file

---
+++ spec/rental_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Rental do
+
+    describe "#initialize" do
+        
+        it "sets the rental date,book,person" do
+            date = '2022-01-01'
+            book = { 'title' => 'Transformation', 'author' => 'Kafka', 'rentals' => [] }
+            person = { 'name' => 'Burak', 'age' => 25, 'parent_permission' => true, 'rentals' => [] }
+            rental = Rental.new(date, book, person)
+            expect(rental.date).to eq date
+            expect(rental.book).to eq book
+            expect(rental.person).to eq person
+        end
+    end
+
+end
